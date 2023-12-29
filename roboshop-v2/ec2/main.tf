56d6db14c0bbcc453809f6424df037d1d0f9a9bc
balusena <balusena12@gmail.com>
2023-12-29 12:25:42 +0000

    roboshop terraform files.

--- roboshop-v2/ec2/main.tf
+++ roboshop-v2/ec2/main.tf
@@ -11,9 +11,9 @@ resource "aws_security_group" "sg" {
   description = "Allow TLS inbound traffic"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
