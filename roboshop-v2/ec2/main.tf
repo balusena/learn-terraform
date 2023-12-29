f8a225486d5f83868f444bd1d91809dd94e052bf
balusena <balusena12@gmail.com>
2023-12-29 03:19:59 +0000

    roboshop terraform files.

--- roboshop-v2/ec2/main.tf
+++ roboshop-v2/ec2/main.tf
@@ -64,7 +64,7 @@ resource "null_resource" "ansible" {
 
     inline = [
       "sudo labauto ansible",
-      "ansible-pull -i localhost, -U https://github.com/balusena/roboshop-ansible/ main.yml -e env=dev -e role_name=${var.name}"
+      "ansible-pull -i localhost, -U https://github.com/balusena/roboshop-ansible main.yml -e env=dev -e role_name=${var.name}"
     ]
   }
 }
