4c8cee48a20b7f63745f35970a97f6dbe9a37c6f
balusena <balusena12@gmail.com>
2023-12-29 01:40:25 +0000

    roboshop ansible files.

---
+++ roboshop-v2/main.tf
@@ -0,0 +1,23 @@
+module "instances" {
+  for_each = var.instances
+  source   = "./ec2"
+  name     = each.key
+}
+
+variable "instances" {
+  default = {
+
+    frontend   = {}
+    mongodb    = {}
+    catalogue  = {}
+    redis      = {}
+    user       = {}
+    cart       = {}
+    mysql      = {}
+    shipping   = {}
+    rabbitmq   = {}
+    payment    = {}
+    dispatch   = {}
+  }
+}
+
